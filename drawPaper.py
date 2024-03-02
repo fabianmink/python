d8b4d31bf69e935027c4822c284d423baf4e6cb9
Fabian Mink THM <fabian.mink@iem.thm.de>
2024-03-02 21:11:02 +0100

    code cleanup

--- drawPaper.py
+++ drawPaper.py
@@ -23,12 +23,11 @@
 #   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 import numpy as np 
-from matplotlib import pyplot as plt 
-import matplotlib as mpl
 import math
+import matplotlib as mpl
+from matplotlib import pyplot as plt 
 
 def drawPaper(fh="none", **kwargs):
-    #print(fig)
     
     if fh == "none":
         #create a new empty figure, if none is given
@@ -45,10 +44,11 @@ def drawPaper(fh="none", **kwargs):
     
     #for key, value in kwargs.items():
     #    print(key + '=' + str(value))
+    
+    #grid resolution (currently fixed to .5cm)
     x_res = 0.5;
     y_res = 0.5;
-        
-        
+                
     x_cm = kwargs.get('x_cm',10) #default value 10
     y_cm = kwargs.get('y_cm',10) 
     
@@ -88,17 +88,16 @@ def drawPaper(fh="none", **kwargs):
     #x_orig = x_cm_orig*x_scale;
     #y_orig = y_cm_orig*y_scale;
     
-    
+    #Scale axes already defined in figure before this function is called
     for axi in fh.axes:
         #print(axi)
         axi.set_position([0,0,1,1])
         axi.patch.set_alpha(0)
         
-        
-        axi.set_xlim((0-x_zero, x_width-x_zero)) #must be adapted acc to scaling
+        axi.set_xlim((0-x_zero, x_width-x_zero)) #adapted according to scaling
         axi.set_ylim((0-y_zero, y_height-y_zero))
         
-        #todo:get max zorder!
+        #todo:get max zorder for axTicksx/y.zorder
         #print(axi)
     
     
@@ -117,33 +116,27 @@ def drawPaper(fh="none", **kwargs):
     axGrid.annotate(y_label, xy=(0, 0), xytext=(x_cm_orig-0.4, y_cm_max+0.15+0.5))
     
     
-    
     #axes for x-y-tick lables
     ticks_offs_x = (x_cm_zero-x_cm_min)*x_scale
     ticks_offs_y = (y_cm_zero-y_cm_min)*y_scale
     ticks_width = (x_cm_max-x_cm_min)*x_scale
     ticks_height = (y_cm_max-y_cm_min)*y_scale
     
-    #axTicks = fh.add_axes([x_cm_min/x_cm,y_cm_min/y_cm,(x_cm_max-x_cm_min)/x_cm,(y_cm_max-y_cm_min)/y_cm])
     axTicksx = fh.add_axes([x_cm_min/x_cm,y_cm_orig/y_cm,(x_cm_max-x_cm_min)/x_cm,(y_cm_max-y_cm_orig)/y_cm])
     axTicksy = fh.add_axes([x_cm_orig/x_cm,y_cm_min/y_cm,(x_cm_max-x_cm_orig)/x_cm,(y_cm_max-y_cm_min)/y_cm])
-    #axTicksx.zorder = -1
+    #axTicksx.zorder = -1 #background
     #axTicksy.zorder = -1
-    axTicksx.zorder = 100
+    axTicksx.zorder = 100  #most likely to foreground
     axTicksy.zorder = 100
     axTicksx.patch.set_alpha(0)
     axTicksy.patch.set_alpha(0)
     
     x_tick_range = (math.floor(x_cm_min-x_cm_zero)*x_cm_tick*x_scale, (math.ceil(x_cm_max-x_cm_zero)+0.1)*x_cm_tick*x_scale) 
     y_tick_range = (math.floor(y_cm_min-y_cm_zero)*y_cm_tick*y_scale, (math.ceil(y_cm_max-y_cm_zero)+0.1)*y_cm_tick*y_scale)
-    #print(x_tick_range)
-    #print(y_tick_range)
-    
+
     axTicksx.xaxis.set_ticks(np.arange(x_tick_range[0], x_tick_range[1], x_cm_tick*x_scale))
-    #axTicksx.xaxis.set_ticks(np.arange(-2, 2, 0.5))
     axTicksx.yaxis.set_ticks(np.array([]))
     axTicksy.xaxis.set_ticks(np.array([]))
-    #axTicksy.yaxis.set_ticks(np.arange(-2, 2, 0.5))
     axTicksy.yaxis.set_ticks(np.arange(y_tick_range[0], y_tick_range[1], y_cm_tick*y_scale))
     axTicksx.axes.set_xlim((-ticks_offs_x, ticks_width-ticks_offs_x)) 
     axTicksx.axes.set_ylim((-ticks_offs_y, ticks_height-ticks_offs_y))
@@ -153,193 +146,10 @@ def drawPaper(fh="none", **kwargs):
     axTicksy.set_frame_on(False)
     
     
-    
     inch_per_cm = 1/2.54
     fh.set_size_inches(x_cm*inch_per_cm,y_cm*inch_per_cm)
       
-    
-    #print(x_cm)
-    
+   
     return(fh)
     
     
-
-def drawPaper_tmp1(dim_val, argfile, valha):
- 
-    #Dimensions
-    x_cm = dim_val['x_cm']
-    y_cm = dim_val['y_cm']
-
-    #Origin KOS
-    x_cm_orig = dim_val['x_cm_orig']; 
-    y_cm_orig = dim_val['y_cm_orig'];
-
-    #Minimum KOS
-    x_cm_min = dim_val['x_cm_min'];
-    y_cm_min = dim_val['y_cm_min'];
-
-    #Maximum KOS
-    x_cm_max = dim_val['x_cm_max'];
-    y_cm_max = dim_val['y_cm_max'];
-
-    #Scaling
-    x_noscale = 1;
-    x_scale = 1;
-    y_noscale = 1;
-    y_scale = 1;
-
-    #Resolution /cm
-    x_res = 0.5;
-    y_res = 0.5;
-
-    #Axis shift
-    x_shift = 0;
-    y_shift = 0;
-
-    if 'x_scale' in dim_val:
-        x_scale = dim_val['x_scale'];
-        x_noscale = 0;
-
-
-    x_label = '';
-    y_label = '';
-
-
-    #Tick resolution
-    x_tickres = 0;
-    y_tickres = 0;
-    #if(isfield(dim_val,'x_tickres'))
-    #    x_tickres = dim_val.x_tickres;
-    #   
-    #    if(isfield(dim_val,'y_tickres'))
-    #    y_tickres = dim_val.y_tickres;
-        
-
-
-    #Calc min/max
-    x_min = x_scale * (x_cm_min-x_cm_orig) + x_shift;
-    y_min = y_scale * (y_cm_min-y_cm_orig) + y_shift;
-
-    #Maximum values
-    x_max = x_scale * (x_cm_max-x_cm_orig) + x_shift;
-    y_max = y_scale * (y_cm_max-y_cm_orig) + y_shift;
-
-    fh = plt.figure();
-
-    #v_cm = (v_pos-x_shift) / x_scale + x_cm_orig;
-    #h_cm = (h_pos-y_shift) / y_scale + y_cm_orig;
-    #plt.plot(np.array([0, 0]), np.array([0, y_cm]))
-    plt.plot(np.array([0, x_cm]), np.array([0, y_cm]))
-
-    #Axes for grid and h/v lines
-    #plot([0 0],[0 y_cm],"k--");
-    #hold on;
-    #for iv = 1:length(v_cm)
-    #plot([v_cm(iv) v_cm(iv)],[y_cm_min y_cm_max],"k--");
-    #end
-    #for ih = 1:length(h_cm)
-    #plot([x_cm_min x_cm_max],[h_cm(ih) h_cm(ih)],"k--");
-    #end
-
-
-    #axes();
-    ha = plt.gca();
-    ha.xaxis.set_ticks(np.arange(0, x_cm, x_res))
-    ha.yaxis.set_ticks(np.arange(0, y_cm, y_res))
-    
-    ha.axes.set_xlim((0, x_cm)) 
-    ha.axes.set_ylim((0, y_cm))
-    ha.axes.grid(axis='both')
-    
-    ha.set_position([0,0,1,1])
-    
-
-    
-    #ha.Position = [0 0 1 1];
-    #set(ha,'Xcolor','none','Ycolor','none','box','off');
-
-    #x-arrow
-    # X = [x_cm_min*1/x_cm  (x_cm_max+0.5)/x_cm];
-    # Y = [y_cm_orig*1/y_cm   y_cm_orig*1/y_cm];
-    # han = annotation('arrow',X,Y);
-    # han.LineWidth = 0.75;
-    # %axis label
-    # %dim = [X(2)-0.02 Y(1)-0.00 .1 .1];
-    # Xanno = (x_cm_max+0.5)/x_cm;
-    # Yanno = (y_cm_orig-0.4)*1/y_cm;
-    # dim = [Xanno Yanno .2 .2];
-    # han = annotation('textbox',dim,'String',x_label,'Margin',0,'VerticalAlignment', 'bottom');
-    # han.LineStyle='none';
-    # hold off;
-
-    # #y-arrow
-    # X = [x_cm_orig*1/x_cm x_cm_orig*1/x_cm];
-    # Y = [y_cm_min*1/y_cm   (y_cm_max+0.5)*1/y_cm];
-    # han = annotation('arrow',X,Y);
-    # han.LineWidth = 0.75;
-    # %axis label
-    # %dim = [X(1) Y(2)-0.05 .1 .1];
-    # Xanno = (x_cm_orig-0.4)*1/x_cm;
-    # Yanno = (y_cm_max+0.6)*1/y_cm;
-    # dim = [Xanno Yanno .2 .2];
-    # han = annotation('textbox',dim,'String',y_label,'Margin',0,'VerticalAlignment', 'bottom');
-    # han.LineStyle='none';
-
-    # #x-axis
-    # if(~x_noscale)
-    # axes()
-    # xlim([x_min,x_max])
-    # ha = gca();
-    # ha.Position = [x_cm_min*1/x_cm y_cm_orig*1/y_cm (x_cm_max-x_cm_min)*1/x_cm 1];
-    # set(ha,'ytick',[],'Ycolor','none','box','off')
-    # %set(ha,'Color','w')
-    # set(ha,'Color','none')
-    # if(x_tickres)
-    #     set(ha,'xtick',ceil(x_min/x_tickres)*x_tickres:x_tickres:floor(x_max/x_tickres)*x_tickres);
-    #     end
-    #     end
-
-    # #y-axis
-    # if(~y_noscale)
-    # axes()
-    # ylim([y_min,y_max])
-    # ha = gca();
-    # ha.Position = [x_cm_orig*1/x_cm y_cm_min*1/y_cm 1 (y_cm_max-y_cm_min)*1/y_cm];
-    # set(ha,'xtick',[],'Xcolor','none','box','off')
-    # %set(ha,'Color','w')
-    # set(ha,'Color','none')
-    # if(y_tickres)
-    # set(ha,'ytick',ceil(y_min/y_tickres)*y_tickres:y_tickres:floor(y_max/y_tickres)*y_tickres);
-    # end
-    # end
-
-    # filename = 'test_kos';
-    # if(nargin >= 2)
-    #     filename = argfile;
-    #     end
-
-    # if(nargin >= 3)
-    # %Value-Axes
-    # valha = copyobj(valha, fh);
-    # axes(valha);
-    
-    # %copyobj(hl,valha);
-    
-    # xlim(valha,[x_min,x_max]);
-    # ylim(valha,[y_min,y_max]);
-    # valha.Position = [x_cm_min*1/x_cm y_cm_min*1/y_cm (x_cm_max-x_cm_min)*1/x_cm (y_cm_max-y_cm_min)*1/y_cm];
-    # set(valha,'xtick',[],'Xcolor','none','box','off');
-    # set(valha,'ytick',[],'Ycolor','none','box','off');
-    # set(valha,'Color','none')
-    # end
-    
-    inch_per_cm = 1/2.54
-
-    fig_size = x_cm*inch_per_cm,y_cm*inch_per_cm
-
-    fh.set_size_inches(fig_size[0],fig_size[1])
-    fh.dpi = 300
-
-
-    plt.savefig("test.png", dpi=300)
-

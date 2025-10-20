6c57e8686331c8b241c264b7bc9ed7a19d0d94bb
Fabian Mink THM <fabian.mink@iem.thm.de>
2025-10-20 18:04:22 +0200

    simplified; clean up code

--- gnss/pos_calc.py
+++ gnss/pos_calc.py
@@ -26,21 +26,33 @@
 #
 
 
+import math
 import numpy as np
 import matplotlib.pyplot as plt
-import drawPaper as dp
 import csv
+#import drawPaper as dp
 
-#FB, Kaiserstraße Ecke Ockstädter Straße
-pos0_lat = 50.33405
-pos0_lon = 8.75242
+#Earth radius
+r = 6371000 #m
 
-#FB THM A2.0.09
-home_lat = 50.32990
-home_lon = 8.75939
 
-#Optional: Get data from file
-with open('gnss_data.csv') as csv_file:
+#Map data
+datafile = 'Friedberg.png' #This has been generated using http://printmaps-osm.de/
+map_size_x = 2000  #m width 
+map_size_y = 3000  #m height
+map_x0 = -522      #m x-offset
+map_y0 = -1347     #m y-offset
+#FB, Kaiserstraße Ecke Ockstaedter Straße
+pos0_lat = 50.33405  #° Must fit to map_x0
+pos0_lon = 8.75242   #° Must fit to map_y0
+
+
+#FB THM A2.0.09 (only if not read from file)
+pos_lat = 50.32990
+pos_lon = 8.75939
+
+#Get lat/lon data from csv-file
+with open('gnss_data_fb.csv') as csv_file:
     home_lat = []
     home_lon = []
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -50,56 +62,54 @@ with open('gnss_data.csv') as csv_file:
             print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
-            #print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
             home_lat.append(float(row[1]))
             home_lon.append(float(row[2]))
             line_count += 1
             
     
-    home_lat = np.array(home_lat)
-    home_lon = np.array(home_lon)
+    pos_lat = np.array(home_lat)
+    pos_lon = np.array(home_lon)
 
 
 
-delta_phi_lat = (home_lat - pos0_lat)/ 360 * 2*np.pi 
-delta_phi_lon = (home_lon - pos0_lon)/ 360 * 2*np.pi * np.cos(pos0_lat/ 360 * 2*np.pi)
+delta_lat = pos_lat - pos0_lat
+delta_lon = pos_lon - pos0_lon
 
-delta_x = delta_phi_lon * 6371000
-delta_y = delta_phi_lat * 6371000
+dx_dlon = r * 2*math.pi/360 * math.cos(pos0_lat * 2.0*math.pi/360)
+dy_dlat = r * 2*math.pi/360
 
-#noise_x = np.random.normal(0, 12, 20)
-#noise_y = np.random.normal(0, 12, 20)
-#delta_x = delta_x + noise_x
-#delta_y = delta_y + noise_y
 
-datafile = 'Friedberg.png'
+delta_x = dx_dlon * delta_lon 
+delta_y = dy_dlat * delta_lat
+
+
 img = plt.imread(datafile)
-
-map_size_x = 2000  #m
-map_size_y = 3000  #m
-map_x0 = -522
-map_y0 = -1347
-
 plt.imshow(img, extent=[map_x0, map_x0+map_size_x, map_y0, map_y0+map_size_y], zorder = -100 )
 
-# Sine function test example
-myDim = {'x_cm_min' : 0.8,
-         'x_cm_zero' : 5, 
-         'y_cm_zero': 10,
-         'x_scale': 100,
-         'y_scale': 100,
-         'x_cm' : 16,
-         'y_cm' : 18,
-         'x_label' : r'$x / \mathrm{m}$',
-         'y_label' : r'$y / \mathrm{m}$',
-         'fg_axes' : True,
-}
 
+plt.plot(delta_x, delta_y, 'r-', lw=1, zorder = 100)
+ax = plt.gca()
+ax.grid()
+ax.set_xlabel(r"$x / \mathrm{m}$")
+ax.set_ylabel(r"$y / \mathrm{m}$")
 
-plt.plot(delta_x, delta_y, 'rx-', lw=1, zorder = 100)
+#fig = ax.get_figure()
 
-fig = plt.gcf()
-dp.drawPaper(fig, **myDim);
+# myDim = {'x_cm_min' : 0.8,
+#          'x_cm_zero' : 5, 
+#          'y_cm_zero': 10,
+#          'x_scale': 100,
+#          'y_scale': 100,
+#          'x_cm' : 16,
+#          'y_cm' : 18,
+#          'x_label' : r'$x / \mathrm{m}$',
+#          'y_label' : r'$y / \mathrm{m}$',
+#          'fg_axes' : True,
+# }
+
+#fig = plt.gcf()
+#dp.drawPaper(fig, **myDim);
+
 plt.savefig("map_fb.png", dpi=300)
 
 plt.show()

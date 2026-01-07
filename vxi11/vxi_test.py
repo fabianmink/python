a744aeedde3c9630f8c93256b71aa141e61dc6a7
Fabian Mink THM <fabian.mink@iem.thm.de>
2026-01-07 14:10:34 +0100

    additional vxi tests and example answers from instruments

--- vxi11/vxi_test.py
+++ vxi11/vxi_test.py
@@ -28,36 +28,103 @@
 # pip install standard-xdrlib
 
 import vxi11
+import struct
+import numpy as np 
+from matplotlib import pyplot as plt
 
-instrScopeIpAddr = "192.168.10.10"   #IP-Address of Instrument
-dScopeSiglent =  vxi11.Instrument(instrScopeIpAddr)
 
+#Agilent Technologies,33220A
 instrFgIpAddr = "192.168.10.11"   #IP-Address of Instrument
 funGenAgilent =  vxi11.Instrument(instrFgIpAddr)
-
-print(dScopeSiglent.ask("*IDN?"))
 print(funGenAgilent.ask("*IDN?"))
-#print(funGenAgilent.ask("APPL?"))
+print(funGenAgilent.ask("APPL?"))
 
-funGenAgilent.write("OUTP OFF")
-funGenAgilent.write("OUTP ON")
 
 funGenAgilent.write("APPL:SIN +1.0e3,+1.0,0.0")  #also enables output
-funGenAgilent.write("APPL:PULS +10.0e3,+1.0,0.0")  #also enables output, no pulse width setting possible!!
+#funGenAgilent.write("APPL:PULS +10.0e3,+1.0,0.0")  #also enables output, no pulse width setting possible!!
 
 #Following steps do not enable output
-funGenAgilent.write("FUNC SIN") 
-funGenAgilent.write("FREQ 4e3") 
-funGenAgilent.write("VOLT 1.0")
-funGenAgilent.write("VOLT:OFFS 0.0")
+#funGenAgilent.write("FUNC SIN") 
+#funGenAgilent.write("FREQ 4e3") 
+#funGenAgilent.write("VOLT 1.0")
+#funGenAgilent.write("VOLT:OFFS 0.0")
 
 #Following steps do not enable output
-funGenAgilent.write("FUNC PULS") 
-funGenAgilent.write("FREQ 1e3") 
-funGenAgilent.write("VOLT 1.0")
-funGenAgilent.write("VOLT:OFFS 0.5")
+#funGenAgilent.write("FUNC PULS") 
+#funGenAgilent.write("FREQ 1e3") 
+#funGenAgilent.write("VOLT 1.0")
+#funGenAgilent.write("VOLT:OFFS 0.5")
 #funGenAgilent.write("FUNC:PULS:WIDT 0.2e-3")  #Pulswidth / s
-funGenAgilent.write("FUNC:PULS:DCYC 40")      #Duty Cycle / %
+#funGenAgilent.write("FUNC:PULS:DCYC 40")      #Duty Cycle / %
+
+#Output off / on 
+#funGenAgilent.write("OUTP OFF")
+#funGenAgilent.write("OUTP ON")
+
+
+#Siglent Technologies,SDS1104X-E
+instrScopeIpAddr = "192.168.10.10"   #IP-Address of Instrument
+dScopeSiglent =  vxi11.Instrument(instrScopeIpAddr)
+print(dScopeSiglent.ask("*IDN?"))
+
+#Channel
+dScopeSiglent.write("C1:VDIV 5.0")
+dScopeSiglent.write("C1:TRA ON")
+dScopeSiglent.write("C2:VDIV 1.0")
+dScopeSiglent.write("C2:TRA ON")
+dScopeSiglent.write("C3:TRA OFF")
+dScopeSiglent.write("C4:TRA OFF")
+
+print(dScopeSiglent.ask("C1:VDIV?"))
+print(dScopeSiglent.ask("C1:OFST?"))
+print(dScopeSiglent.ask("C1:ATTN?"))
+print(dScopeSiglent.ask("C1:UNIT?"))
+print(dScopeSiglent.ask("C1:CPL?"))
+print(dScopeSiglent.ask("C1:TRA?"))
+
+#Timebase
+dScopeSiglent.write("TDIV 2e-4")
+print(dScopeSiglent.ask("TDIV?"))
+print(dScopeSiglent.ask("TRDL?"))
+
+#Trigger
+dScopeSiglent.write("C1:TRLV 0.0")
+dScopeSiglent.write("C1:TRSL POS")
+print(dScopeSiglent.ask("TRCP?"))
+print(dScopeSiglent.ask("TRLV?"))
+print(dScopeSiglent.ask("TRSE?"))
+#print(dScopeSiglent.ask("TRSL?")) #Does not work
+
+#Sampling
+sara = dScopeSiglent.ask("SARA?")
+fs = float((sara.split()[1]).split("S")[0])
+print(dScopeSiglent.ask("SARA?"))
+
+#print(dScopeSiglent.ask("SANU?")) #Does not work
+
+
+#Setup Waveform
+dScopeSiglent.write("WFSU SP,1000,NP,10000,FP,0")
+
+#Query waveform
+#Raw query must be used, as return contain bytes that can not be utf-8 decoded
+raw_query_bytes = "C1:WF? DAT2".encode("utf-8") 
+response_bytes = dScopeSiglent.ask_raw(raw_query_bytes)
+wf_bytes = response_bytes[22:-2]  #remove header and end
+wf = []
+for value in struct.iter_unpack("b", wf_bytes): #interprete each byte as b="signed char"
+    wf.append(value[0])
+    
+u_ch1 = np.array(wf) / 128.0 * 5.0;  #scale to 5V/div
+    
+#sx = len(ch)
+#t = np.linspace(-1/fs*sx/2, 1/fs*sx/2, sx)    
+
+plt.plot(wf, 'r-', lw=1)    
+ 
+  
+
+
 
 
 

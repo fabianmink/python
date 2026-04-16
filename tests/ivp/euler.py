b9dd65a776dde33cd869103e30b3ab1bc68e884d
Fabian Mink THM <fabian.mink@iem.thm.de>
2026-04-16 10:02:10 +0200

    Euler method solver added

---
+++ tests/ivp/euler.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Euler-Solver for scipy.integrate.solve_ivp
+
+@author: Fabian Mink
+"""
+
+from scipy.integrate import OdeSolver, DenseOutput
+from warnings import warn
+
+
+def euler_step(f, h, t_k, x_k):
+    der = f(t_k, x_k)
+    x_k = x_k + h * der
+    t_k = t_k + h
+    return t_k, x_k
+
+
+def warn_extraneous(extraneous):
+    if extraneous:
+        warn("The following arguments are unused in RK4: "
+             f"{', '.join(f'`{x}`' for x in extraneous)}.",
+             stacklevel=3)
+
+class eulerSolver(OdeSolver):
+
+    def __init__(self, fun, t0, y0, t_bound, vectorized=False,
+                 stepsize=None, **extraneous):
+        
+        warn_extraneous(extraneous)
+        
+        super().__init__(fun, t0, y0, t_bound, vectorized,
+                         support_complex=False)
+        
+        #TODO: Validate stepsize
+        #if stepsize is None:
+            #TODO: error
+        # .. other validations ...
+            
+        self.stepsize = stepsize    
+            
+        #DEBUGGING INFO
+        #print("__init__ of myrk4")
+
+
+    #A solver must implement a private method _step_impl(self) 
+    #which propagates a solver one step further. 
+    #It must return tuple (success, message), where success is a 
+    #boolean indicating whether a step was successful, 
+    #and message is a string containing description of a failure
+    #if a step failed or None otherwise.
+    def _step_impl(self):
+        t = self.t
+        y = self.y
+
+        #t_new = t + self.stepsize
+        #y_new = y #dummy for testing, no change of y
+        t_new, y_new = euler_step(self.fun, self.stepsize, t, y)
+
+        self.t = t_new
+        self.y = y_new
+       
+        #DEBUGGING INFO
+        #print("_step_impl of myrk4" )
+        #print("stepsize: " + str(self.step_size) )
+        #print("t:" + str(self.t) + " t_old:" + str(self.t_old))
+        #print("y: " + str(self.y) )
+        #print("n: " + str(self.n) )
+        #print("status:" + str(self.status))
+        #print("direction:" + str(self.direction))
+        #print("")
+        
+        return True, None
+
+    #A solver must implement a private method _dense_output_impl(self),
+    #which returns a DenseOutput object covering the last successful step.
+    def _dense_output_impl(self):
+        #DEBUGGING INFO
+        print("_dense_output_impl of myrk4" )
+        #TODO: Keine Ahnung ob das sinnvoll implementiert ist; die Funktion wird offensichtlich
+        #durch solve_ivp nie aufgerufen
+        return DenseOutput(self.t_old, self.t)

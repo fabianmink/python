d9162f741505b41164d664c6c5f081f732df515c
Fabian Mink THM <fabian.mink@iem.thm.de>
2026-05-13 09:06:35 +0200

    Sources according to en60617 added

--- tests/schemdraw/diode_circuit_draw.py
+++ tests/schemdraw/diode_circuit_draw.py
@@ -8,11 +8,17 @@ Created on Fri Mar 20 08:15:27 2026
 import schemdraw
 import schemdraw.elements as elm
 
+import sources_en60617 as mysrcs
+
+
+
 elm.style(elm.STYLE_IEC)
 
     
 with schemdraw.Drawing(file='diode_circuit.png') as d:
-    src = d.add(elm.SourceV().up())
+    #src = d.add(elm.SourceV().up())
+    src = d.add(mysrcs.SourceU().up())
+    #src = d.add(mysrcs.SourceI().up())
     d.add(elm.Line().up())
     I = d.add(elm.Line().right())
     d.add(elm.Resistor().down().label('200 Ω'))
@@ -21,6 +27,6 @@ with schemdraw.Drawing(file='diode_circuit.png') as d:
     
     d.add(elm.Arrow().at( src.end + schemdraw.util.Point((-0.8,-0.5)) ).to(src.start + schemdraw.util.Point((-0.8,0.5))).label('2 V', ofst=[0,0]))
     d.add(elm.Arrow().at(I.start).to(I.end + schemdraw.util.Point((-0.2,0.0))).label('I', ofst=[1,0]))
-    d.add(elm.Arrow().at( D.start + schemdraw.util.Point((+0.6,-1)) ).to(D.end + schemdraw.util.Point((+0.6,1))).label('$U_\mathrm{d}$', ofst=[0.,0.8]))
+    d.add(elm.Arrow().at( D.start + schemdraw.util.Point((+0.6,-1)) ).to(D.end + schemdraw.util.Point((+0.6,1))).label(r'$U_\mathrm{d}$', ofst=[0.,0.8]))
     
     
---
+++ tests/schemdraw/sources_en60617.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 08:45:57 2026
+
+@author: Fabian Mink
+"""
+
+import schemdraw.elements as elm
+import schemdraw.segments as seg
+import math
+
+gap = (math.nan, math.nan)  # Put a gap in a path
+
+class SourceU(elm.Element2Term):
+    ''' Generic source element '''
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.segments.append(seg.Segment([(0, 0), (1, 0)]))
+        self.segments.append(seg.SegmentCircle((0.5, 0), 0.5,))
+        self.elmparams['theta'] = 90
+        
+class SourceI(elm.Element2Term):
+    ''' Generic source element '''
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.segments.append(seg.Segment([(0, 0), (0, 0), gap, (1, 0), (1, 0)]))
+        self.segments.append(seg.Segment([(0.5, 0.5), (0.5, -0.5)]))
+        self.segments.append(seg.SegmentCircle((0.5, 0), 0.5,))
+        self.elmparams['theta'] = 90

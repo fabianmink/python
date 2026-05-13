0965984f68268d18b68a72e9afe9d1b1370dbfb5
Fabian Mink THM <fabian.mink@iem.thm.de>
2026-05-13 07:25:53 +0200

    diode circuit added

---
+++ tests/schemdraw/diode_circuit_draw.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 08:15:27 2026
+
+@author: Fabian Mink
+"""
+
+import schemdraw
+import schemdraw.elements as elm
+
+elm.style(elm.STYLE_IEC)
+
+    
+with schemdraw.Drawing(file='diode_circuit.png') as d:
+    src = d.add(elm.SourceV().up())
+    d.add(elm.Line().up())
+    I = d.add(elm.Line().right())
+    d.add(elm.Resistor().down().label('200 Ω'))
+    D = d.add(elm.Diode().down().label('1N4148'))
+    d.add(elm.Line().left())
+    
+    d.add(elm.Arrow().at( src.end + schemdraw.util.Point((-0.8,-0.5)) ).to(src.start + schemdraw.util.Point((-0.8,0.5))).label('2 V', ofst=[0,0]))
+    d.add(elm.Arrow().at(I.start).to(I.end + schemdraw.util.Point((-0.2,0.0))).label('I', ofst=[1,0]))
+    d.add(elm.Arrow().at( D.start + schemdraw.util.Point((+0.6,-1)) ).to(D.end + schemdraw.util.Point((+0.6,1))).label('$U_\mathrm{d}$', ofst=[0.,0.8]))
+    
+    

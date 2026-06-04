641c645500e0014ef78e870ab71fdefa113e6d22
Fabian Mink THM <fabian.mink@iem.thm.de>
2026-06-04 18:51:18 +0200

    noninverting amplifier added

--- tests/schemdraw/noninv_amp_draw .py
+++ tests/schemdraw/noninv_amp_draw .py
@@ -12,34 +12,39 @@ schemdraw.config(inches_per_unit=0.8/2.54, lw = 1.0, fontsize=11)
 elm.style(elm.STYLE_IEC)
 
     
-with schemdraw.Drawing(file='inv_amp_circuit.png', dpi=300) as d:
+with schemdraw.Drawing(file='noninv_amp_circuit.png', dpi=300) as d:
    
-    op = elm.Opamp(leads=True)
+    op = elm.Opamp(leads=True).flip()
     
-    R1 = elm.Resistor().at(op.in1).left().idot().label(r'$R_{1}$', loc='bot')
+    
+    elm.Line().at(op.in1).down(d.unit*2/3)
+    elm.Line().tox(op.out)
+    
+    
+    R2 = elm.Resistor().toy(op.out).idot().label(r'$R_{2}$', loc='bot')
+    elm.Dot()
+    R1 = elm.Resistor().down().at(R2.start).label(r'$R_{1}$', loc='bot')
+    
+    d.push()
+    elm.Dot()
+    gndline = elm.Line().left(d.unit*5/3)
+    
+    
+    Uin_neg = elm.Dot(open=True)
+    
+    elm.Line().tox(gndline.end).at(op.in2)
     
     Uin_pos = elm.Dot(open=True)
     
-    d.move(0,-d.unit)
-    Uin_neg = elm.Dot(open=True)
+    d.pop()
     
-    gndline = elm.Line().right(d.unit*8/3)
+    gndline = elm.Line().right(d.unit*2/3)
     
     Uout_neg = elm.Dot(open=True)
     
-    
-    elm.Line().toy(gndline.start).at(op.in2)
-    elm.Dot()
-    elm.Vss()
-    
-    
     elm.Line().tox(gndline.end).at(op.out)
-    Uout_pos = elm.Dot(open=True)
     
-    elm.Line().up(d.unit/2).at(op.in1)
-    elm.Resistor().tox(op.out).label(r'$R_2$')
-    elm.Line().toy(op.out).dot()
-       
+    Uout_pos = elm.Dot(open=True)       
     
     d.add(elm.Arrow().at(Uin_pos.start + schemdraw.util.Point((0,-0.3))).to(Uin_neg.start + schemdraw.util.Point((0,0.3))).label(r'$U_\mathrm{e}$'))
     d.add(elm.Arrow().at(Uout_pos.start + schemdraw.util.Point((0,-0.3))).to(Uout_neg.start + schemdraw.util.Point((0,0.3))).label(r'$U_\mathrm{a}$', loc='bottom'))

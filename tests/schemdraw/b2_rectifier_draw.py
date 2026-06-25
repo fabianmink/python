874b2eaf9377b94b3e9f8f7542bfdbe3529222ea
Fabian Mink THM <fabian.mink@iem.thm.de>
2026-06-25 11:27:46 +0200

    circuit improved

--- tests/schemdraw/b2_rectifier_draw.py
+++ tests/schemdraw/b2_rectifier_draw.py
@@ -24,13 +24,13 @@ with schemdraw.Drawing(file='b2_rectifier_circuit.png', dpi=300) as d:
     
     uin_pos = elm.Dot()
     
-    D1 = elm.Diode().up().label('D1')
+    D1 = elm.Diode().up().label(r'$D_1$')
     elm.Line().right()
     elm.Dot()
     elm.Line().right()
-    uout_pos = elm.Dot()
+    urect_pos = elm.Dot()
     elm.Line().down().at(uin_pos.start)
-    D2 = elm.Diode().reverse().label('D2')
+    D2 = elm.Diode().reverse().label(r'$D_3$')
     
     elm.Line().right()
     elm.Dot()
@@ -40,51 +40,30 @@ with schemdraw.Drawing(file='b2_rectifier_circuit.png', dpi=300) as d:
     elm.Line().right().at(src.start)
     elm.Line().right()
     
-    uin_neg = elm.Dot()
+    urect_neg = elm.Dot()
     
     elm.Line().up()  
-    D3 = elm.Diode().up().label('D3')
-    D4 = elm.Diode().reverse().down().at(uin_neg.start).label('D4')
+    D3 = elm.Diode().up().label(r'$D_2$')
+    D4 = elm.Diode().reverse().down().at(urect_neg.start).label(r'$D_4$')
     
-    elm.Line().down().at(uout_pos.start)
+    elm.Line().down().at(urect_pos.start)
     elm.Capacitor().down().label(r'$C$')
     elm.Line().down()
     
-    elm.Line().right().at(uout_pos.start)
+    elm.Line().right().at(urect_pos.start)
+    uout_pos = elm.Dot(open=True)
     
+    elm.Line().right()
+        
     elm.Line().down()
-    elm.Resistor().down().label(r'$R$')
+    elm.Resistor().down().label(r'$R_\mathrm{L}$', loc='bottom')
     elm.Line().down()
+    
     elm.Line().left()
     
+    uout_neg = elm.Dot(open=True)
     
-    # op = elm.Opamp(leads=True)
+    elm.Line().left()
     
-    # R1 = elm.Resistor().at(op.in1).left().idot().label(r'$R$', loc='bot')
+    elm.Arrow().at(uout_pos.start + schemdraw.util.Point((0,-0.3))).to(uout_neg.start + schemdraw.util.Point((0,0.3))).label(r'$u_\mathrm{out}$', ofst=[0,1.25])
     
-    # Uin_pos = elm.Dot(open=True)
-    
-    # d.move(0,-d.unit)
-    # Uin_neg = elm.Dot(open=True)
-    
-    # gndline = elm.Line().right(d.unit*8/3)
-    
-    # Uout_neg = elm.Dot(open=True)
-    
-    
-    # elm.Line().toy(gndline.start).at(op.in2)
-    # elm.Dot()
-    # elm.Vss()
-    
-    
-    # elm.Line().tox(gndline.end).at(op.out)
-    # Uout_pos = elm.Dot(open=True)
-    
-    # elm.Line().up(d.unit/2).at(op.in1)
-    # elm.Capacitor().tox(op.out).label(r'$C$')
-    # elm.Line().toy(op.out).dot()
-       
-    
-    # d.add(elm.Arrow().at(Uin_pos.start + schemdraw.util.Point((0,-0.3))).to(Uin_neg.start + schemdraw.util.Point((0,0.3))).label(r'$u_\mathrm{in}$'))
-    # d.add(elm.Arrow().at(Uout_pos.start + schemdraw.util.Point((0,-0.3))).to(Uout_neg.start + schemdraw.util.Point((0,0.3))).label(r'$u_\mathrm{out}$', loc='bottom'))
- 
